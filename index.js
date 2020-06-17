d908311c40616672eca635815db27e073869ab7e
Allan Oliveira <a.oliveira@curtin.edu.au>
2020-06-17 16:34:44 +0800

    Use axios to query google.com

---
+++ index.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+axios.interceptors.request.use(function (config) {
+    config.metadata = { startTime: new Date()}
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+    response.config.metadata.endTime = new Date()
+    response.duration = response.config.metadata.endTime - response.config.metadata.startTime
+    return response;
+}, function (error) {
+    error.config.metadata.endTime = new Date();
+    error.duration = error.config.metadata.endTime - error.config.metadata.startTime;
+    return Promise.reject(error);
+});
+
+axios.get('https://www.google.com')
+  .then(response => {
+    //console.log(response);
+    console.log(`Status: ${response.status}`);
+    console.log(`Status: ${response.headers['content-type']}`);
+    console.log(response.duration)
+  })
+  .catch(error => {
+    console.log(error);
+  });
